c45470b5a06baaec98f59d8d55e57d890ac4749f
LUVINA\nguyenminhduc2 <nguyenminhduc2@luvina.net>
2024-05-10 16:58:43 +0700

    3.34.22 continue...

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -73,21 +73,85 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def height(self):
+        if self is None:
+            return 0
+        return 1 + max(TreeNode.height(self.left), TreeNode.height(self.right))
 
-node0 = TreeNode(3)
-node1 = TreeNode(4)
-node2 = TreeNode(5)
+    def size(self):
+        if self is None:
+            return 0
+        return 1 + TreeNode.size(self.left) + TreeNode.size(self.right)
 
-node0.left = node1
-node0.right = node2
+    def traverse_in_order(self):
+        if self is None:
+            return []
+        return (
+            TreeNode.traverse_in_order(self.left)
+            + [self.key]
+            + TreeNode.traverse_in_order(self.right)
+        )
 
-tree = node0
+    def display_keys(self, space="\t", level=0):
+        # If the node is empty
+        if self is None:
+            print(space * level + "∅")
+            return
+
+        # If the node is a leaf
+        if self.left is None and self.right is None:
+            print(space * level + str(self.key))
+            return
+
+        # If the node has children
+        TreeNode.display_keys(self.right, space, level + 1)
+        print(space * level + str(self.key))
+        TreeNode.display_keys(self.left, space, level + 1)
+
+    def to_tuple(self):
+        if self is None:
+            return None
+        if self.left is None and self.right is None:
+            return self.key
+        return TreeNode.to_tuple(self.left), self.key, TreeNode.to_tuple(self.right)
+
+    def __str__(self):
+        return "BinaryTree <{}>".format(self.to_tuple())
+
+    def __repr__(self):
+        return "BinaryTree <{}>".format(self.to_tuple())
+
+    @staticmethod
+    def parse_tuple(data):
+        if data is None:
+            node = None
+        elif isinstance(data, tuple) and len(data) == 3:
+            node = TreeNode(data[1])
+            node.left = TreeNode.parse_tuple(data[0])
+            node.right = TreeNode.parse_tuple(data[2])
+        else:
+            node = TreeNode(data)
+        return node
+
+
+# node0 = TreeNode(3)
+# node1 = TreeNode(4)
+# node2 = TreeNode(5)
+
+# node0.left = node1
+# node0.right = node2
+
+# tree = node0
 # print(tree.key)
 # print(tree.left.key)
 # print(tree.right.key)
 
 
 tree_tuple = ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8)))
+tree = TreeNode.parse_tuple(tree_tuple)
+
+# print(tree.size())
+tree.display_keys('  ')
 
 
 def parse_tuple(data):
@@ -106,25 +170,73 @@ def parse_tuple(data):
 tree2 = parse_tuple(((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8))))
 
 
+def tree_to_tuples(node):
+    if node is None:
+        return None
+    if node.left is None and node.right is None:
+        return node.key
+    return tree_to_tuples(node.left), node.key, "aaa", tree_to_tuples(node.right)
+
+
+# print(tree_to_tuples(tree2))
+
+
 # print(tree2.key)
 # print(tree2.left.left.key, tree2.left.right, tree2.right.left.key, tree2.right.right.key)
-def display_keys(node, space="\t", level=0):
-    # print(node.key if node else None, level)
 
-    # If the node is empty
+
+# def display_keys(node, space="\t", level=0):
+#     # print(node.key if node else None, level)
+
+#     # If the node is empty
+#     if node is None:
+#         print(space * level + "∅")
+#         return
+
+#     # If the node is a leaf
+#     if node.left is None and node.right is None:
+#         print(space * level + str(node.key))
+#         return
+
+#     # If the node has children
+#     display_keys(node.right, space, level + 1)
+#     print(space * level + str(node.key))
+#     display_keys(node.left, space, level + 1)
+
+
+# display_keys(tree2, "   ")
+
+
+def traverse_in_order(node):
     if node is None:
-        print(space * level + "∅")
+        return []
+    return traverse_in_order(node.left) + [node.key] + traverse_in_order(node.right)
+
+
+def pre_order(node, L):
+    if node is None:
         return
 
-    # If the node is a leaf
-    if node.left is None and node.right is None:
-        print(space * level + str(node.key))
+    L.append(node.key)
+
+    pre_order(node.left, L)
+    pre_order(node.right, L)
+
+    return L
+
+
+def post_order(node, L):
+    if node is None:
         return
 
-    # If the node has children
-    display_keys(node.right, space, level + 1)
-    print(space * level + str(node.key))
-    display_keys(node.left, space, level + 1)
+    L.append(node.key)
+
+    post_order(node.right, L)
+    post_order(node.left, L)
+
+    return L
 
 
-display_keys(tree2, '   ')
+# print(traverse_in_order(tree2))
+# print(pre_order(tree2, []))
+# print(post_order(tree2, []))
