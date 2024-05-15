8e295d26da076e52552d1d947873a6d44148d5e1
LUVINA\nguyenminhduc2 <nguyenminhduc2@luvina.net>
2024-05-15 17:06:41 +0700

    hash table...

---
+++ binary_search_tree/dictionaries_hash_tables.py
@@ -0,0 +1,22 @@
+class HashTable:
+    def insert(self, key, value):
+        pass
+
+    def find(self, key):
+        pass
+
+    def update(self, key, value):
+        pass
+
+    def list_all(self):
+        pass
+
+
+MAX_HASH_TABLE_SIZE = 4096
+
+data_list = [None] * MAX_HASH_TABLE_SIZE
+
+print(len(data_list))
+
+for item in data_list:
+    assert item == None
