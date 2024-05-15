357bd7120d2d56bc2cdb7633e06974b981f8aec6
LUVINA\nguyenminhduc2 <nguyenminhduc2@luvina.net>
2024-05-15 17:17:46 +0700

    commit

--- binary_search_tree/dictionaries_hash_tables.py
+++ binary_search_tree/dictionaries_hash_tables.py
@@ -16,7 +16,28 @@ MAX_HASH_TABLE_SIZE = 4096
 
 data_list = [None] * MAX_HASH_TABLE_SIZE
 
-print(len(data_list))
+# print(len(data_list))
 
-for item in data_list:
-    assert item == None
+# for item in data_list:
+#     assert item == None
+
+
+def get_index(data_list, a_string):
+    # Variable to store the result (updated after each iteration)
+    result = 0
+
+    for a_character in a_string:
+        # Convert the character to a number (using ord)
+        a_number = ord(a_character)
+        # Update result by adding the number
+        result += a_number
+
+    # Take the remainder of the result with the size of the data list
+    print(">>> result: ",result)
+    list_index = result % len(data_list)
+    return list_index
+
+
+
+
+
