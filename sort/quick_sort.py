76e249a2fcd7799a08fe8002821dc7fc31767b7c
LUVINA\nguyenminhduc2 <nguyenminhduc2@luvina.net>
2024-05-24 17:03:06 +0700

    7.52.00 continue...

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -134,4 +134,25 @@ def lcs_recursive(seq1, seq2, idx1=0, idx2=0):
 
 
 print(lcs_recursive("serendipitous", "precipitation"))
-print(lcs_recursive([1, 3, 5, 6, 7, 2, 5, 2, 3], [6, 2, 4, 7, 1, 5, 6, 2, 3]))
+# print(lcs_recursive([1, 3, 5, 6, 7, 2, 5, 2, 3], [6, 2, 4, 7, 1, 5, 6, 2, 3]))
+
+
+def lcs_memo(seq1, seq2):
+    memo = {}
+
+    def recurse(idx1=0, idx2=0):
+        key = (idx1, idx2)
+
+        if key in memo:
+            return memo[key]
+        elif idx1 == len(seq1) or idx2 == len(seq2):
+            memo[key] = 0
+        elif seq1[idx1] == seq2[idx2]:
+            memo[key] = 1 + recurse(idx1 + 1, idx2 + 1)
+        else:
+            memo[key] = max(recurse(idx1 + 1, idx2), recurse(idx1, idx2 + 1))
+        return memo[key]
+    return recurse(0, 0)
+
+
+print(lcs_memo("serendipitous", "precipitation"))
--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-def merge(nums1, nums2, depth=0):
-    print("  " * depth, "merge:", nums1, nums2)
-    i, j, merged = 0, 0, []
-    while i < len(nums1) and j < len(nums2):
-        if nums1[i] <= nums2[j]:
-            merged.append(nums1[i])
-            i += 1
-        else:
-            merged.append(nums2[j])
-            j += 1
-    return merged + nums1[i:] + nums2[j:]
+memo = {}
+key1 = (8,9)
+key2 = (4,4)
+memo[key1] = 3
+memo[key2] = 9
 
 
-def merge_sort(nums, depth=0):
-    print("  " * depth, "merge_sort:", nums)
-    if len(nums) < 2:
-        return nums
-    mid = len(nums) // 2
-    return merge(
-        merge_sort(nums[:mid], depth + 1), merge_sort(nums[mid:], depth + 1), depth + 1
-    )
-
-
-merge_sort([5, -12, 2, 6, 1, 23, 7, 7, -12])
+print((7,9) in memo)
